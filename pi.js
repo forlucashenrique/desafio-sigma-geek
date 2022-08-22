0401d3d1f2f27a1a41ab5de1b453f29b08326501
Lucas Henrique <forlucashenrique@gmail.com>
2022-08-22 03:07:57 -0300

    código

---
+++ pi.js
@@ -0,0 +1,55 @@
+const fs = require('fs')
+
+function getFirstPalindromicPrime(){
+
+    fs.readFile('./piOneMilionOfDigits.txt', 'utf-8', (err, data) => {
+        if(err){
+            console.log(err)
+            return
+        }
+        getNumWithNineAlg(data)
+    })
+    
+}   
+
+function getNumWithNineAlg(number){
+    for(let i = 0; i < number.length; i++){
+        const numWithNineAlg = number.slice(i, i + 9)
+        if(isPalindromic(numWithNineAlg) && !numWithNineAlg.startsWith(0) && isPrime(numWithNineAlg)) {
+            console.log(numWithNineAlg)
+            return
+        }
+            
+        
+    }
+
+}
+
+
+function isPalindromic(num){
+
+    let numInvert = [...num].reverse()
+
+    for(let i = 0; i < num.length; i++){
+        if(num[i] !== numInvert[i]) return false
+    }
+
+    return true
+}
+
+
+function isPrime(number){
+    const num = Number(number)
+    let count = 0
+    for (let i = 1; i <= num; i++){
+        if(num % i === 0){
+            count++
+        }
+    }
+    if(count === 2)
+        return true
+    
+    return false
+}
+
+getFirstPalindromicPrime()
